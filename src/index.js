5380d39aea6d53609235f710a91bbea9114edac1
Alberto Valero <alberto.valero@bq.com>
2018-07-12 01:03:29 +0200

    first example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,38 @@
-import message from './message';
-import './index.scss';
+import * as THREE from 'three';
 
-const paragraph = document.createElement('p');
-paragraph.innerHTML = message;
+let camera;
+let scene;
+let renderer;
 
-document.body.prepend(paragraph);
+let geometry;
+let material;
+let mesh;
+
+function init() {
+  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+  camera.position.z = 1;
+
+  scene = new THREE.Scene();
+
+  geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+  material = new THREE.MeshNormalMaterial();
+
+  mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.02;
+
+  renderer.render(scene, camera);
+}
+
+init();
+animate();
